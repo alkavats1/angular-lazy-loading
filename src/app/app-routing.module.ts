1e8418d215d888a37be3b3ea37e1d8728579585d
alkavats1 <alka.vats@knoldus.com>
2021-08-05 13:39:42 +0530

    Angular Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) }, { path: 'lazy-loading2', loadChildren: () => import('./lazy-loading2/lazy-loading2.module').then(m => m.LazyLoading2Module) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {Routes} from "@angular/router";
+import {LazyLoading2Module} from "./lazy-loading2/lazy-loading2.module";
 
+const routes: Routes = [
+  {
+    path: 'lazy-loading',
+    loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule)
+  },
+  {
+    path: 'lazy-loading2',
+    loadChildren: () => import('./lazy-loading2/lazy-loading2.module').then(m => m.LazyLoading2Module)
+  }
+];
 @NgModule({
   declarations: [
     AppComponent
